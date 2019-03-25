d219eed74d3456088e9b6d8e5e3f5dd4a8febf70
Joel Gullander <swagger@dynamicdog.se>
2019-03-25 22:27:57 +0100

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    GIT_USER: process.env.GIT_USER || 'Joelgullander',
-    GIT_PASSWORD: process.env.GIT_PASSWORD || 'Cuma4617',
-    swaggers: process.env.SWAGGER_LIST ? process.env.SWAGGER_LIST.split(',') : ['http://localhost:5000']
+    GIT_USER: process.env.GIT_USER || null,
+    GIT_PASSWORD: process.env.GIT_PASSWORD || null,
+    swaggers: process.env.SWAGGER_LIST ? process.env.SWAGGER_LIST.split(',') : []
 }
